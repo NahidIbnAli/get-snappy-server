ab2598754b8a53aecd2c9f55a6bd9a0ffa92fe25
Nahid Ibn Ali <nahidibnali7@gmail.com>
2022-11-10 01:38:47 +0600

    created get api for fetching specific service's data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -35,6 +35,13 @@ async function run() {
       const services = await cursor.toArray();
       res.send(services);
       console.log(services);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
     });
   } finally {
   }
