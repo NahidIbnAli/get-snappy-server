25a4d65bfd2bdde4d6300f08f6fdcb5412ce883e
Nahid Ibn Ali <nahidibnali7@gmail.com>
2022-11-09 10:42:22 +0600

    installed mongodb and connected server with the database

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = `mongodb+srv://getSnappyDbUser:8DWUqt7WtqpYEsWn@cluster0.wgjxpn1.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 // middleware
 app.use(cors());
